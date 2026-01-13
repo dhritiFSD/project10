d0cdd36d2b7cb743c8dceb7b8702a296c7b863e3
Gajjar Dhriti <dhriti.g1012@gmail.com>
2026-01-13 14:35:05 +0530

    Add files via upload

---
+++ access.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class math
+{
+	public:
+	void sum(int a , int b)
+	{
+		cout<<a+b<<endl;
+	}	
+};
+int main()
+{
+	math addition;
+	addition.sum(10,20);
+	
+}
